b40cabd25ff3c0ef413d1fff2438fc4b04d0c51c
Ian Koh <ian_koh93@hotmail.com>
2018-03-08 16:37:41 -0800

    Switch from Full Graphics to Smart Controller 2004LCD

--- Marlin-1.1.x/Marlin/Configuration.h
+++ Marlin-1.1.x/Marlin/Configuration.h
@@ -1325,7 +1325,7 @@
 //
 // Note: Usually sold with a white PCB.
 //
-//#define REPRAP_DISCOUNT_SMART_CONTROLLER
+#define REPRAP_DISCOUNT_SMART_CONTROLLER
 
 //
 // GADGETS3D G3D LCD/SD Controller
@@ -1339,7 +1339,7 @@
 // RepRapDiscount FULL GRAPHIC Smart Controller
 // http://reprap.org/wiki/RepRapDiscount_Full_Graphic_Smart_Controller
 //
-#define REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER
+//#define REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER
 
 //
 // MakerLab Mini Panel with graphic
